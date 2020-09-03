5b25829604afc6540af5198bcbc9ec31a5c7393f
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-09-03 11:36:39 +0900

    isuue1〜3

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,24 +34,18 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-<<<<<<< HEAD
       # wday_num = Date.today
       wday_num = Date.today.wday + x #Date.today.wdayを利用して添字となる数値を得る
-=======
 
-      wday_num = Date.today[0, 1, 2, 3, 4, 5, 6]#Date.today.wdayを利用して添字となる数値を得る
->>>>>>> 3bd32c59fe89704ed8339ee99b154e108b56dc95
+
       #もしもwday_numが7以上であれば、7を引く
       if wday_num >= 7#条件式を記述
         wday_num = wday_num - 7
-<<<<<<< HEAD
       end
+
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :wday => wdays[(@todays_date+x).wday], :plans => today_plans}
-=======
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, wday: => (@todays_date+x).wday}
-
->>>>>>> 3bd32c59fe89704ed8339ee99b154e108b56dc95
+ 
       @week_days.push(days)
     end
   end
